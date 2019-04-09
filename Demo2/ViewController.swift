6f823b98fc90946fedf4f6dd5dba59587fec635c
manhnd <Manh.it.it.it@gmail.com>
2019-04-09 15:01:48 +0700

    Initial Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by Digital on 4/9/19.
+//  Copyright © 2019 Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
